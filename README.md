9bd39d4da86173020502a22b92db2e2847f55e46
ShadowCrypt97 <91398635+ShadowCrypt97@users.noreply.github.com>
2024-07-25 21:50:25 -0500

    chore: first version of Text Encrypter app (#1)
    * html template structure
    
    * added images and inputs
    
    * added icon
    
    * updated buttons for anchors on index.html and section for main tag
    
    * chore: added link tag pointing to style.css file
    
    * chore: added css styles
    
    * chore: added google fonts and fix some css stuffs
    
    * chore: added function encryptMessage
    
    * chore: added button copy and function
    
    * fix: responsivity
    
    * chore: add decrypted function
    
    * chore: added copy function
    
    * fix: decrypt function removing empty response and showing text decrypted
    
    * chore: added README File

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # text-encrypter-app
 Here there is a project created in HTML, CSS and Javascript with the purpose of encrypt and decrypt the text provide by the user.
+
+## Description
+
+This pull request introduces a new feature to the application: text encryption and decryption. Users can now input text directly on the screen and choose to either encrypt or decrypt it using a simple interface.
+
+### Key Features:
+- **Text Encryption**: Converts plaintext into ciphertext using a custom algorithm.
+- **Text Decryption**: Converts ciphertext back into plaintext.
+- **User Interface**: A clean and intuitive interface for inputting text and viewing results.
+
+### Technologies Used:
+- **HTML5**: For structuring the web page.
+- **CSS3**: For styling the application and ensuring a responsive design.
+- **JavaScript**: For implementing the encryption and decryption logic and handling user interactions.
+
+### Screenshots:
+![Desencriptado](https://github.com/user-attachments/assets/5d7e5537-3ae1-4dce-bca1-5d6df390849c)
+![encriptado](https://github.com/user-attachments/assets/a4edab62-6073-4c58-93fa-2ba48499f9c1)
+
+
+### Testing:
+- **Manual Testing**: The feature has been manually tested on multiple browsers to ensure compatibility.
+- **Edge Cases**: Tested with various inputs, including empty strings and special characters.
+
+### Additional Notes:
+- Ensure to review the encryption algorithm for security and performance.
+- Future improvements could include adding support for different encryption methods and enhancing the UI/UX.
---
+++ script.js
@@ -0,0 +1,103 @@
+const encryptBtn = document.querySelector("#encrypt");
+const decryptBtn = document.querySelector("#decrypt");
+const decryptBtnContainer = document.querySelector("#decrypt-btn-container");
+const inputText = document.querySelector("#input-textarea");
+const responseContainer = document.querySelector("#output-res-container");
+const outputContainer = document.querySelector("#response-container");
+const responseEmptyContainer = document.querySelector("#response-empty-container");
+const answerPanel = document.querySelector("#answer-panel");
+const responseText = document.querySelector("#response");
+
+function cleanTextArea(){
+    if(inputText.value === "Write something here")
+        inputText.value = "";
+}
+
+function restoreTextArea(){
+    if(inputText.value === "")
+        inputText.value = "Write something here";
+}
+
+function assignTextResponse(texto) {
+    responseText.innerHTML = texto;
+    return;
+}
+
+function encrypt(){
+    let encryptedText = ""
+    if(inputText.value === "Write something here" || inputText.value === ""){
+        document.querySelector("#answer-panel").append(responseEmptyContainer);
+        outputContainer.setAttribute("style", "display:none;");
+        decryptBtnContainer.setAttribute("class", "decrypt-button decrypt-button-disabled col-lg-4 col-sm-12 col-md-12 col-xs-12");
+    }
+    else{
+        responseEmptyContainer.remove();
+        outputContainer.removeAttribute("style");
+        decryptBtnContainer.setAttribute("class", "decrypt-button col-lg-4 col-sm-12 col-md-12 col-xs-12");
+        encryptedText = encryptMessage(inputText.value);
+        assignTextResponse(encryptedText);
+        inputText.value = "Write something here";
+    }    
+}
+
+function encryptMessage(message){
+    if(/[A-Z]/.test(message)){
+        alert("Error: The text must be on lowercase");
+    }else if(containsSpecialChars(message)){
+        alert("Error: The text must not has special characters");
+    }
+    else{
+        let rules = encryptRules();
+        for (var vowel in rules) {
+            var regex = new RegExp(vowel, 'g');
+            message = message.replace(regex, rules[vowel]);
+        }
+        return message;
+    }
+}
+
+function containsSpecialChars(text) {
+    var regex = /[áéíóúÁÉÍÓÚñÑüÜ!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    var output = regex.test(text);
+    return output;
+}
+
+
+function encryptRules(){
+    let rules = {
+        "e": "enter",
+        "i": "imes",
+        "a": "ai",
+        "o": "ober",
+        "u": "ufat"
+    };
+    return rules;
+}
+
+function decrypt(){
+    if(inputText.value === "Write something here" || inputText.value === ""){
+        outputContainer.setAttribute("style", "display:none;");
+        decryptBtnContainer.setAttribute("class", "decrypt-button decrypt-button-disabled col-lg-4 col-sm-12 col-md-12 col-xs-12");
+        answerPanel.appendChild(responseEmptyContainer);
+    }else{
+        responseEmptyContainer.remove();
+        outputContainer.removeAttribute("style");
+        decryptedText = decryptMessage(inputText.value);
+        assignTextResponse(decryptedText);
+        inputText.value = "Write something here";
+    }
+}
+
+function decryptMessage(encryptedMessage){
+    let rules = encryptRules();
+    for (var vowel in  rules) {
+        var regex = new RegExp(rules[vowel], 'g');
+        encryptedMessage = encryptedMessage.replace(regex, vowel);
+    }
+    return encryptedMessage;
+}
+
+async function copy(){
+    await navigator.clipboard.writeText(responseText.textContent);
+    alert("message copied successfully on the clipboard!");
+}
